4ed9a123cc046ccd2e2beb7d8ac3118da532d49b
liuxiaoru <liuxiaoru666@163.com>
2020-08-23 16:02:50 +0800

    路由导航首位，登录校验，页面重定向

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//插件
 import './plugins/element.js'
 import './plugins/mPlugins.js'
 import '@/assets/css/reset.css'
+//权限
+import '@/permission.js';
+
 
 
 new Vue({
